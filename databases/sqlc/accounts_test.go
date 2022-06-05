3207825639113736bff1b011b8dbc1d878d6f871
Emediong Francis <emediongfrancis@gmail.com>
2022-06-05 15:13:18 +0100

    Write Read_Account test

--- databases/sqlc/accounts_test.go
+++ databases/sqlc/accounts_test.go
@@ -3,12 +3,13 @@ package databases
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/EmediongFrancis/Amabilis-Bank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func CreateRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomBalance(),
@@ -23,4 +24,23 @@ func TestCreateAccount(t *testing.T) {
 	require.Equal(t, arg.Currency, account.Currency)
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	CreateRandomAccount(t)
+}
+
+func TestReadAccount(t *testing.T) {
+	account := CreateRandomAccount(t)
+
+	accountRead, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, accountRead)
+	require.Equal(t, account.ID, accountRead.ID)
+	require.Equal(t, account.Owner, accountRead.Owner)
+	require.Equal(t, account.Balance, accountRead.Balance)
+	require.Equal(t, account.Currency, accountRead.Currency)
+	require.WithinDuration(t, account.CreatedAt, accountRead.CreatedAt, time.Second)
 }
